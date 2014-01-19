ada9d61ccd5735a050e58fd7d9b568a38de4eead
egy186 <egy186@yahoo.co.jp>
2014-01-19 16:20:43 +0900

    Auto increment the AssemblyVersion

--- egyViewport2013/egyViewport2013.cs
+++ egyViewport2013/egyViewport2013.cs
@@ -18,21 +18,15 @@ namespace egyViewport2013
         [DataMember]
         public double imageOpacity = 0.2;
     }
-    
+
     /// <summary>
     /// Adornment class for background image in right hand of the viewport
     /// </summary>
     class egyViewport2013
     {
         private Image _image;
-        private BitmapImage _bitmapImage;
         private IWpfTextView _view;
         private IAdornmentLayer _adornmentLayer;
-
-        private string _fileNameConfig = "config.json";
-        private egyConfig _config;
-        private string assemblyLoc = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        private DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(egyConfig));
 
         /// <summary>
         /// Read a image and attaches an event handler to the layout changed event
@@ -42,30 +36,32 @@ namespace egyViewport2013
         {
             _view = view;
 
+            string fileNameConfig = "config.json";
+            string assemblyLoc = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             Stream mStream = new MemoryStream();
-            FileStream fs = new FileStream(Path.Combine(assemblyLoc, _fileNameConfig), FileMode.Open);
+            FileStream fs = new FileStream(Path.Combine(assemblyLoc, fileNameConfig), FileMode.Open);
+            DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(egyConfig));
             long fLength = fs.Length;
             byte[] fByte = new byte[fLength];
             fs.Read(fByte, 0, (int)fLength);
             mStream.Write(fByte, 0, (int)fLength);
             fs.Dispose();
-
             mStream.Position = 0;
-            _config = (egyConfig)dcjs.ReadObject(mStream);
+            egyConfig vConfig = (egyConfig)dcjs.ReadObject(mStream);
             mStream.Dispose();
 
-            _bitmapImage = new BitmapImage();
-            _bitmapImage.BeginInit();
-            _bitmapImage.UriSource = new Uri(Path.Combine(assemblyLoc, _config.imageFileName), UriKind.Absolute);
-            _bitmapImage.EndInit();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(Path.Combine(assemblyLoc, vConfig.imageFileName), UriKind.Absolute);
+            bitmapImage.EndInit();
 
             _image = new Image();
-            _image.Width = _bitmapImage.PixelWidth;
-            _image.Height = _bitmapImage.PixelHeight;
-            _image.Opacity = _config.imageOpacity;
-            _image.Source = _bitmapImage;
+            _image.Width = bitmapImage.PixelWidth;
+            _image.Height = bitmapImage.PixelHeight;
+            _image.Opacity = vConfig.imageOpacity;
+            _image.Source = bitmapImage;
             _image.Stretch = Stretch.Uniform;
-
 
             //Grab a reference to the adornment layer that this adornment should be added to
             _adornmentLayer = view.GetAdornmentLayer("egyViewport2013");
